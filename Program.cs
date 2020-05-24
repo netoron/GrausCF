38029092cd93b455b1099294daba34e1af30289a
netoron <61334943+netoron@users.noreply.github.com>
2020-05-24 19:21:04 -0300

    Add files via upload
    Adicionando programa de conversão de temperatura

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string divisao;
+            double total;
+            double mais = 1.8;
+            double menos = 32;
+            double conteudo;
+            
+            Console.WriteLine("Digite a temperatura em °C para ser convertido em °F");
+            divisao = Console.ReadLine();
+            
+            conteudo = Convert.ToDouble(divisao);
+
+            total = conteudo * mais + menos;
+            
+            Console.WriteLine($"Conversão é {total}°F");
+
+
+
+        }
+    }
+}
